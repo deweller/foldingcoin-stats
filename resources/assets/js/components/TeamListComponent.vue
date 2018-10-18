fffe1c2b785f409e2a0aab54dec9616cc6d5e929
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-10-18 14:33:14 -0400

    remove table CSS classes to fix formatting

--- resources/assets/js/components/TeamListComponent.vue
+++ resources/assets/js/components/TeamListComponent.vue
@@ -42,13 +42,13 @@
                         <th><a @click="pager.toggleSort('name', 'asc')" href="#sort">
                             <i v-if="paging.sort == 'name'" :class="{fa: true, 'fa-sort-down': paging.sortDirection == 'desc', 'fa-sort-up': paging.sortDirection == 'asc'}"></i> 
                             Name</a></th>
-                        <th class="d-none -d-md-block"><a @click="pager.toggleSort('number', 'asc')" href="#sort">
+                        <th><a @click="pager.toggleSort('number', 'asc')" href="#sort">
                             <i v-if="paging.sort == 'number'" :class="{fa: true, 'fa-sort-down': paging.sortDirection == 'desc', 'fa-sort-up': paging.sortDirection == 'asc'}"></i> 
                             Team Number</a></th>
                         <!--<th><a @click="pager.toggleSort('dayPoints', 'desc')" href="#sort">
                             <i v-if="paging.sort == 'dayPoints'" :class="{fa: true, 'fa-sort-down': paging.sortDirection == 'desc', 'fa-sort-up': paging.sortDirection == 'asc'}"></i> 
                             24h Points</a></th>-->
-                        <th class="d-none d-sm-block"><a @click="pager.toggleSort('weekPoints', 'desc')" href="#sort">
+                        <th><a @click="pager.toggleSort('weekPoints', 'desc')" href="#sort">
                             <i v-if="paging.sort == 'weekPoints'" :class="{fa: true, 'fa-sort-down': paging.sortDirection == 'desc', 'fa-sort-up': paging.sortDirection == 'asc'}"></i> 
                             7d Points</a></th>
                         <th><a @click="pager.toggleSort('allPoints', 'desc')" href="#sort">
@@ -60,10 +60,10 @@
                 <tbody>
                     <tr v-for="team in teams">
                         <td><a :href="'/team/'+team.number">{{ team.name }}</a></td>
-                        <td class="d-none -d-md-block"><a :href="'/team/'+team.number">{{ team.number }}</a></td>
+                        <td><a :href="'/team/'+team.number">{{ team.number }}</a></td>
 
                         <!--<td>{{ team.dayPoints | points }}</td>-->
-                        <td class="d-none d-sm-block">{{ team.weekPoints | points }}</td>
+                        <td>{{ team.weekPoints | points }}</td>
                         <td>{{ team.allPoints | points }}</td>
                     </tr>
                 </tbody>
